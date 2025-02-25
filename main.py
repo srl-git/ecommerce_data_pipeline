88226e9a82b45267618b28c520bcd7f53a7dd296
srl-git <samrosslaye@gmail.com>
2025-02-25 00:53:42 +0000

    Added clean_missing_prices func

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from dotenv import load_dotenv
 from google_cloud_storage import list_bucket_contents, download_from_bucket, download_from_bucket_as_bytes
 
 load_dotenv()
+
 # today = datetime.now().strftime('%Y-%m-%d')
-# # print(today)
+# bucket_name = os.getenv('bucket_name')
 # todays_reports = [blob.name for blob in list_bucket_contents(bucket_name) if '2025-02-20' in blob.name]
 
 # for report in todays_reports:
@@ -32,6 +33,8 @@ expected_cols = ['order_line_id', 'order_id', 'user_id', 'item_sku', 'qty', 'ite
 
 df = pd.read_csv('Order_report_2025-02-19-1.csv')
 
+prices = {'SUMO004': 21, 'GAS006': 20}
+
 def check_columns(df: pd.DataFrame, expected_cols: list[str]) -> pd.DataFrame:
 
     cols = df.columns.to_list()
@@ -43,17 +46,25 @@ def check_columns(df: pd.DataFrame, expected_cols: list[str]) -> pd.DataFrame:
         print(f'Unexpected extra column(s) in data source: \n{extra_cols}')
     if missing_cols:
         raise ValueError(f'Missing column(s) in data source: \n{missing_cols}')
-    # if not missing_cols and not extra_cols:
-    #     df = df[expected_cols]
     return df
 
-def clean_date_formats(df: pd.DataFrame, date_column) -> pd.DataFrame:
+def clean_date_formats(df: pd.DataFrame, date_column: str) -> pd.DataFrame:
 
-    df[date_column] = pd.to_datetime(df['date_created'], errors='coerce', format='mixed', dayfirst=True,)
-    incorrect_dates_df = df[df['date_created'].isna()]
+    df[date_column] = pd.to_datetime(df[date_column], errors='coerce', format='mixed', dayfirst=True,)
+    incorrect_dates_df = df[df[date_column].isna()]
     if not incorrect_dates_df.empty:
-        print(f'Unable to parse dates from {len(incorrect_dates_df)} row(s). \n {incorrect_dates_df}')
+        print(f'Unable to parse dates from {len(incorrect_dates_df)} row(s). \n {incorrect_dates_df}\n')
+    df[date_column] = df[date_column].ffill().bfill()
+    return df
+
+def clean_missing_prices(df: pd.DataFrame) -> pd.DataFrame:
+    # print(df[df.isna().any(axis=1)])
+    df['item_price'] = df.groupby('item_sku')['item_price'].transform(lambda x: x.ffill().bfill()) # fill price from other rows
+    df['item_price'] = df['item_price'].fillna(df['item_sku'].map(prices)) # fill price from database as dict
+    # print(df[df.isna().any(axis=1)])
     return df
 
 df = check_columns(df, expected_cols)
 df = clean_date_formats(df, 'date_created')
+df = clean_missing_prices(df)
+print(df)
